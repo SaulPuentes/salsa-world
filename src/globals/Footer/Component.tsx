f335d5b05f91cf0e6b77c01403d32dc144a7e375
SaulPuentes <saul.puentess@gmail.com>
2025-04-25 01:22:38 -0600

    Footer refinement

--- src/globals/Footer/Component.tsx
+++ src/globals/Footer/Component.tsx
@@ -2,34 +2,37 @@ import { getCachedGlobal } from '@/utilities/getGlobals'
 import Link from 'next/link'
 import React from 'react'
 
-import type { Footer } from '@/payload-types'
+import type { Footer, Media } from '@/payload-types'
 
 import { CMSLink } from '@/components/Link'
 import { Logo } from '@/components/Logo/Logo'
 import { SocialLinks } from '@/components/SocialLinks'
+import Image from 'next/image'
 
 export async function Footer() {
   const footerData: Footer = await getCachedGlobal('footer', 1)()
 
   const shortcuts = footerData?.shortcuts || []
   const contact = footerData?.contact
+  const featuredEventImage = footerData?.featuredEvent?.image as Media
 
+  console.log('featuredEventImage', featuredEventImage)
   return (
-    <footer className="mt-auto bg-purple text-white flex-col">
-      <div className="container py-8 gap-8 flex flex-col md:flex-row md:justify-between">
-        <Link className="flex items-center" href="/">
+    <footer className="mt-auto bg-purple text-white flex-col text-md md:text-lg">
+      <div className="container py-8 px-5 gap-8 md:flex flex-col md:flex-row md:justify-between">
+        <Link className='hidden md:block' href="/">
           <Logo />
         </Link>
 
-        <div className="flex flex-col-reverse items-start md:flex-row gap-10">
+        <div className="flex flex-col items-start md:flex-row gap-10">
           <nav className="flex flex-col gap-4">
-            <h1 className='text-2xl uppercase'>Accesos directos</h1>
+            <h1 className='text-2xl md:text-[28px] uppercase'>Accesos directos</h1>
             {shortcuts.map(({ link }, i) => {
               return <CMSLink className="text-white" key={i} {...link} />
             })}
           </nav>
-          <div className="flex flex-col gap-4 max-w-[270px]">
-            <h1 className='text-2xl uppercase'>Donde encontrarnos</h1>
+          <div className="flex flex-col gap-4 max-w-[300px]">
+            <h1 className='text-2xl md:text-[28px] uppercase'>Donde encontrarnos</h1>
               <p className="text-white">
                 {contact?.address}
               </p>
@@ -41,25 +44,33 @@ export async function Footer() {
               </p>
               <SocialLinks color='text-pink' />
           </div>
-          <div className='flex flex-col gap-4 text-white'>
-            <h1 className='text-2xl uppercase'>Evento destacado del mes</h1>
-            <h3 className='text-lg'>
+          <div className='flex flex-col gap-4 text-white max-w-[380px]'>
+            <h1 className='text-2xl md:text-[28px] uppercase'>Evento destacado del mes</h1>
+            <Image
+              src={featuredEventImage?.url || ''}
+              alt={featuredEventImage?.alt || ''}
+              width={378}
+              height={143}
+              objectPosition='center'
+              objectFit='cover'
+            />
+            <h3 className='text-lg uppercase'>
               Salsa Fest
             </h3>
             <p>
-              Salsa World Festival 2024
+              Frase resumida del evento para enganchar que accedan a apuntarse
             </p>
           </div>
         </div>
       </div>
-      <hr className="container block w-full border-t border-border border-white" />
+      <hr className="container block w-full border-t border-border border-white w-[90%] md:w-full" />
       <div className='container flex-col justify-center items-center pt-8 pb-10'>
-        <div className="flex justify-center gap-6 flex-wrap text-pink">
+        <div className="flex flex-col md:flex-row items-center justify-center gap-6 flex-wrap text-pink text-sm">
           <Link href="/aviso-legal" className="hover:underlin">Aviso Legal</Link>
           <Link href="/politica-privacidad" className="hover:underline">Política de Privacidad</Link>
           <Link href="/politica-cookies" className="hover:underline">Política de Cookies</Link>
         </div>
-        <div className="mt-4 text-white text-center font-lilita text-lg">
+        <div className="mt-10 md:mt-4 text-white text-center font-lilita text-lg">
           © {new Date().getFullYear()} Salsa World. Todos los derechos reservados.
         </div>
       </div>
